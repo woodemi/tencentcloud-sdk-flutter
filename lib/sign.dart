f949338828a6a6624de6a3b28d6916ddeb441634
wangkun <wangkun@woodemi.com>
2019-08-14 16:50:34 +0800

    Add `SignerV3`
    Signed-off-by: wangkun <wangkun@woodemi.com>

--- lib/sign.dart
+++ lib/sign.dart
@@ -1,7 +1,10 @@
 import 'dart:convert';
+import 'dart:io';
 import 'dart:math';
 
+import 'package:convert/convert.dart';
 import 'package:crypto/crypto.dart';
+import 'package:intl/intl.dart';
 import 'package:meta/meta.dart';
 
 import 'common.dart';
@@ -55,4 +58,81 @@ class Signer {
     var digest = Hmac(sha1, utf8.encode(secretKey)).convert(utf8.encode(plaintext));
     return base64.encode(digest.bytes);
   }
+}
+
+class SignerV3 {
+  final Credential credential;
+
+  final signatureMethod = SignatureMethod.Tc3HmacSHA256;
+
+  final String service;
+
+  final bool ignorePayload;
+
+  SignerV3(this.credential, this.service, [this.ignorePayload = false])
+      : assert(credential != null), assert(service != null);
+
+  static const unsignedPayload = 'UNSIGNED-PAYLOAD';
+
+  static final dateFormat = DateFormat('yyyy-MM-dd');
+
+  ///  If [ignorePayload] is true, [unsignedPayload] is used instead of [payload]
+  Map<String, Object> sign(String uriPrefix, Map<String, String> plainHeaders, String payload) {
+    assert(plainHeaders.containsKey(HttpHeaders.hostHeader));
+    assert(plainHeaders.containsKey(HttpHeaders.contentTypeHeader));
+
+    var headerPairs = plainHeaders.entries
+        .map((e) => MapEntry(e.key.toLowerCase().trim(), e.value.trim()))
+        .toList()..sort((a, b) => a.key.compareTo(b.key));
+    var canonicalHeaders = headerPairs.map((e) => '${e.key}:${e.value}').join('\n') + '\n';
+    var signedHeaders = headerPairs.map((e) => e.key).toList().join(';');
+    var canonicalRequest = [
+      uriPrefix,
+      canonicalHeaders,
+      signedHeaders,
+      _computeSha256(ignorePayload ? unsignedPayload : payload), // HashedRequestPayload
+    ].join('\n');
+    var hashedCanonicalRequest = _computeSha256(canonicalRequest);
+
+    var secondsSinceEpoch = getTimestamp();
+    var date = dateFormat.format(DateTime.fromMillisecondsSinceEpoch(secondsSinceEpoch * Duration.millisecondsPerSecond).toUtc());
+    var credentials = [date, service, 'tc3_request'];
+    var credentialScope = credentials.join('/');
+    var stringToSign = [
+      signatureMethod.name,
+      secondsSinceEpoch, // RequestTimestamp
+      credentialScope,
+      hashedCanonicalRequest,
+    ].join('\n');
+
+    String signature = _signWithScope(credentials, stringToSign);
+
+    var authInfo = [
+      'Credential=${credential.secretId}/$credentialScope',
+      'SignedHeaders=$signedHeaders',
+      'Signature=$signature',
+    ].join(', ');
+
+    return {
+      ...plainHeaders,
+      'Authorization': '${signatureMethod.name} $authInfo',
+      if (ignorePayload) 'X-TC-Content-SHA256': unsignedPayload,
+      'X-TC-Timestamp': secondsSinceEpoch,
+    };
+  }
+
+  @visibleForTesting
+  int getTimestamp() => DateTime.now().millisecondsSinceEpoch ~/ Duration.millisecondsPerSecond;
+
+  String _signWithScope(List<String> credentials, String stringToSign) {
+    var initialKey = utf8.encode('TC3${credential.secretKey}');
+    var secretSigning = credentials.fold(initialKey, (lastKey, credential) {
+      return _computeHmacSha256(lastKey, credential);
+    });
+    return hex.encode(_computeHmacSha256(secretSigning, stringToSign));
+  }
+
+  String _computeSha256(String plaintext) => sha256.convert(utf8.encode(plaintext)).toString();
+
+  List<int> _computeHmacSha256(List<int> key, String plaintext) => Hmac(sha256, key).convert(utf8.encode(plaintext)).bytes;
 }
--- test/test_sign.dart
+++ test/test_sign.dart
@@ -1,3 +1,5 @@
+import 'dart:io';
+
 import 'package:flutter_test/flutter_test.dart';
 import 'package:tencent_cloud_api/common.dart';
 import 'package:tencent_cloud_api/sign.dart';
@@ -25,15 +27,39 @@ void testSign() {
   test('sign ${SignatureMethod.HmacSHA256}', () {
     // TODO
   });
+
+  test('sign ${SignatureMethod.Tc3HmacSHA256}', () {
+    var testSignerV3 = TestSignerV3(Credential('AKIDz8krbsJ5yKBZQpn74WFkmLPx3EXAMPLE', 'Gu5t9xGARNpq86cd98joQYCN3EXAMPLE'), 'cvm');
+    testSignerV3.timestamp = 1551113065;
+
+    var payload = '{"Limit": 1, "Filters": [{"Values": ["\\u672a\\u547d\\u540d"], "Name": "instance-name"}]}';
+
+    var uriPrefix = ['POST', '/', ''/*CanonicalQueryString*/,].join('\n');
+    var plainHeaders = {
+      HttpHeaders.hostHeader: 'cvm.tencentcloudapi.com',
+      HttpHeaders.contentTypeHeader: 'application/json; charset=utf-8',
+    };
+    var signedHeaders = testSignerV3.sign(uriPrefix, plainHeaders, payload);
+    expect(signedHeaders['Authorization'], 'TC3-HMAC-SHA256 Credential=AKIDz8krbsJ5yKBZQpn74WFkmLPx3EXAMPLE/2019-02-25/cvm/tc3_request, SignedHeaders=content-type;host, Signature=72e494ea809ad7a8c8f7a4507b9bddcbaa8e581f516e8da2f66e2c5a96525168');
+  });
 }
 
 class TestSigner extends Signer {
   TestSigner(Credential credential, [SignatureMethod signatureMethod]): super(credential, signatureMethod);
-  
+
   int nonce;
-  
+
   @override
   int getNonce() => nonce;
+
+  int timestamp;
+
+  @override
+  int getTimestamp() => timestamp;
+}
+
+class TestSignerV3 extends SignerV3 {
+  TestSignerV3(Credential credential, String service) : super(credential, service);
 
   int timestamp;
 
