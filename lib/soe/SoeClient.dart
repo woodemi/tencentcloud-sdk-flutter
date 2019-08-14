79f036631a46a7ab8647d4efbf38b11bdb2bde4e
wangkun <wangkun@woodemi.com>
2019-08-14 19:09:35 +0800

    Add `transmitOralProcessWithInit` to SoeClient
    Signed-off-by: wangkun <wangkun@woodemi.com>

--- lib/soe/SoeClient.dart
+++ lib/soe/SoeClient.dart
@@ -1,11 +1,15 @@
+import 'dart:convert';
+
 import '../AbstractClient.dart';
 import '../common.dart';
 import 'OralProcess.dart';
+import 'VoiceFile.dart';
+import 'struct.dart';
 
 class SoeClient extends AbstractClient {
   SoeClient(Credential credential) : super(
     endpoint: 'soe.tencentcloudapi.com',
-    apiVersion: '2018-07-24',
+    apiVersion: '2018-07-2 4',
     credential: credential,
   );
 
@@ -13,11 +17,11 @@ class SoeClient extends AbstractClient {
     return {
       'SessionId': oralProcess.sessionId,
       'RefText': oralProcess.refText,
-      'WorkMode': oralProcess.workMode,
-      'EvalMode': oralProcess.evalMode,
+      'WorkMode': oralProcess.workMode.value,
+      'EvalMode': oralProcess.evalMode.value,
       'ScoreCoeff': oralProcess.scoreCoeff,
-      if (oralProcess.storageMode != null) 'StorageMode': oralProcess.storageMode,
-      if (oralProcess.serverType != null) 'ServerType': oralProcess.serverType,
+      if (oralProcess.storageMode != null) 'StorageMode': oralProcess.storageMode.value,
+      if (oralProcess.serverType != null) 'ServerType': oralProcess.serverType.value,
     };
   }
 
@@ -26,5 +30,18 @@ class SoeClient extends AbstractClient {
     var actionParams = _buildOralProcessParams(oralProcess);
 
     return (await postRequest(action, actionParams)).toString();
+  }
+
+  Future<String> transmitOralProcessWithInit(OralProcess oralProcess, VoiceFile voiceFile) async {
+    var actionParams = {
+      ..._buildOralProcessParams(oralProcess),
+      'SeqId': 1,
+      'IsEnd': Boolean.yes.value,
+      'VoiceFileType': voiceFile.voiceFileType.value,
+      'VoiceEncodeType': voiceFile.voiceEncodeType.value,
+      'UserVoiceData': base64.encode(voiceFile.userVoiceData),
+    };
+
+    return (await postRequestV3('TransmitOralProcessWithInit', actionParams)).toString();
   }
 }
